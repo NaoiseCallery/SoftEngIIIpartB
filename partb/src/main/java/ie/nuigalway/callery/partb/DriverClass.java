18aa471cea702c9045a68f02bbedbc36c044c9be
Naoise Callery <naoisecallery@gmail.com>
2018-10-27 09:44:27 +0100

    Started part b

---
+++ partb/src/main/java/ie/nuigalway/callery/partb/DriverClass.java
@@ -0,0 +1,33 @@
+package ie.nuigalway.callery.partb;
+
+/**
+Part b) acts as a driver project creating a handful of fictional students with mock data,
+adding the students to a number of modules and adding a number of modules to a course.
+Further on, the program should print out a list of all the courses, their respective modules,
+and all students, their usernames, assigned modules and the course(s) they are registered
+for to the console. 
+ */
+
+public class DriverClass {
+    public static void main(String[] args){
+        
+    }
+    
+    public void createStudent(){
+        
+    }
+    
+    public void addToModule(){
+        
+    }
+    
+    public void addToCourse() {
+        
+    }
+    
+    public void printResults(){
+        //list of courses
+        //their respective modules
+        //Students for a module + usernames
+    }
+}
